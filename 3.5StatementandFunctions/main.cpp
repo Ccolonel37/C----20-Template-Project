cb0b3a904381499d89a6237d8e3ae5bcb94cb441
ccolonel37 <ccolonel37@gmail.com>
2022-09-06 18:56:39 -0500

    3.5StatementandFunctions

---
+++ 3.5StatementandFunctions/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+
+int main(){
+   
+    int first_number {3}; //Statement
+    int second_number {7};
+
+    std::cout << "First number :" << first_number << std::endl;
+    std::cout << "Second number :" << second_number << std::endl;
+
+    return 0;
+
+   
+}
