d11097e51f7d41c40a42b72c93e144ae00cb92af
ccolonel37 <ccolonel37@gmail.com>
2022-10-09 19:28:01 -0500

    Template project

---
+++ 3.7CppDevWorkflow/main.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+int addNumber (int first_param, int second_param){
+
+    int result = first_param + second_param;
+
+    return result;
+}
+
+int main(){
+   
+    int first_number {3}; //Statement
+    int second_number {7};
+
+    std::cout << "First number : " << first_number << std::endl;
+    std::cout << "Second number : " << second_number << std::endl;
+
+    int sum = first_number + second_number;
+    std::cout << "Sum : " << sum << std::endl;
+
+    sum = addNumber (9,7);
+    std::cout << "Sum : " << sum << std::endl;
+
+
+
+    return 0;
+
+   
+}
